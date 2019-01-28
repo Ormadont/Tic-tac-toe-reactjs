b00938ab6e5826f7899cdd8face245db14e5a226
Ormadont <evgeniy1demyanov@gmail.com>
2019-01-28 13:14:37 +0400

    Declaring a Winner

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-
     state = {
         squares: Array(9).fill(null),
         xIsNext: true,
@@ -22,13 +21,33 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        if (squares[i] === null) {
+        if (squares[i] === null && !this.calculateWinner(this.state.squares)) {
             squares[i] = this.state.xIsNext ? 'X' : 'O'
             this.setState({
                 squares: squares,
                 xIsNext: !this.state.xIsNext,
             });
         }
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if ((squares[a] === squares[b]) && (squares[b] === squares[c])) {
+                return squares[a]
+            }
+        }
+        return null;
     }
 
     renderSquare(i) {
@@ -41,8 +60,13 @@ class Board extends React.Component {
     }
 
     render() {
-        // const status = 'Next player: X';
-        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = `Winner: ${winner}`;
+        } else {
+            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        }
         return (
             <div>
                 <div className="status">{status}</div>
