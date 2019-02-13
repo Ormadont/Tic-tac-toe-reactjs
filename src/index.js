966085f14ced5cf045710aa333e31d3da4bcfc93
Ormadont <evgeniy1demyanov@gmail.com>
2019-02-13 11:22:16 +0400

    Picking a Key

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ class Game extends React.Component {
                 'Перейти к ходу №' + move :
                 'Перейти к началу игры';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => { this.jumpTo(move) }}>{desc}</button>
                 </li>
             )
